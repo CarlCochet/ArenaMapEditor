bf6e55f8391dafaf7796186df7f0af0edead7639
Carl <carl.cochet@gmail.com>
2024-11-29 20:29:09 +0100

    Begin reading gfx bytes

--- Scripts/Data/GfxData.cs
+++ Scripts/Data/GfxData.cs
@@ -6,7 +6,16 @@ using System.IO.Compression;
 
 public class GfxData
 {
-    private readonly List<string> _ignoreFiles = ["META-INF", "coord", "groups.lib"]; 
+    private readonly List<string> _ignoreFiles = ["META-INF", "coord", "groups.lib"];
+    public const int MapWidth = 1024;
+    public const int MapHeight = 576;
+    
+    private int _coordMinX = int.MaxValue;
+    private int _coordMinY = int.MaxValue;
+    private short _coordMinZ = short.MaxValue;
+    private int _coordMaxX = int.MinValue;
+    private int _coordMaxY = int.MinValue;
+    private short _coordMaxZ = short.MinValue;
 
     public GfxData(string path, string id)
     {
@@ -31,7 +40,36 @@ public class GfxData
 
     private void ReadBytes(BinaryReader reader)
     {
+        _coordMinX = reader.ReadInt32();
+        _coordMinY = reader.ReadInt32();
+        _coordMinZ = reader.ReadInt16();
+        _coordMaxX = reader.ReadInt32();
+        _coordMaxY = reader.ReadInt32();
+        _coordMaxZ = reader.ReadInt16();
         
+        var baseX = reader.ReadInt32();
+        var baseY = reader.ReadInt32();
+        var maxZ = reader.ReadInt16() & 0xFFFF;
+
+        for (var z = 0; z < maxZ; z++)
+        {
+            var minX = baseX + (reader.ReadByte() & 255);
+            var maxX = baseX + (reader.ReadByte() & 255);
+            var minY = baseY + (reader.ReadByte() & 255);
+            var maxY = baseY + (reader.ReadByte() & 255);
+
+            for (var x = minX; x < maxX; x++)
+            {
+                for (var y = minY; y < maxY; y++)
+                {
+                    var elementCount = reader.ReadByte() & 255;
+                    for (var elementIndex = 0; elementIndex < elementCount; elementIndex++)
+                    {
+                        
+                    }
+                }
+            }
+        }
     }
 
     public class Partition
