b5848fa1684457a74acc0341e3f4122a01e2e27c
Carl <carl.cochet@gmail.com>
2024-12-15 19:12:23 +0100

    Finish parsing music data

--- Scripts/Data/PlaylistData.cs
+++ Scripts/Data/PlaylistData.cs
@@ -1,17 +1,26 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 public class PlaylistData
 {
     public short Id { get; set; }
 
+    public List<MusicData> Musics1 { get; set; } = [];
+    public List<MusicData> Musics2 { get; set; } = [];
+    
+    public MusicData Music1 { get; set; }
+    public MusicData Music2 { get; set; }
+    public MusicData Music3 { get; set; }
+    public MusicData Music4 { get; set; }
+
     public void Load(BinaryReader reader)
     {
         Id = reader.ReadInt16();
-        LoadMusic(reader);
-        LoadMusic(reader);
-        LoadMusic(reader);
+        LoadMusic(reader, 1);
+        LoadMusic(reader, 2);
+        LoadMusic(reader, 3);
     }
 
     public void Save(BinaryWriter writer)
@@ -19,14 +28,50 @@ public class PlaylistData
         
     }
 
-    private void LoadMusic(BinaryReader reader)
+    private void LoadMusic(BinaryReader reader, int iteration)
     {
         var musicCount = reader.ReadInt16();
         for (var i = 0; i < musicCount; i++)
         {
             var music = new MusicData();
             music.Load(reader);
+            OrderDispatch(iteration, music);
         }
+    }
+
+    private void OrderDispatch(int iteration, MusicData music)
+    {
+        switch ((int)music.Order)
+        {
+            case -2:
+                Music4 = music;
+                break;
+            case -1:
+                Music3 = music;
+                break;
+            case 0:
+                if (iteration == 1) Music1 = music;
+                if (iteration == 2) Music2 = music;
+                break;
+            default:
+                AddMusicToList(iteration, music);
+                break;
+        }
+    }
+
+    private void AddMusicToList(int iteration, MusicData music)
+    {
+        if (iteration == 1)
+        {
+            Musics1.Add(music);
+            Musics1.Sort((x, y) => (int)(x.Id - y.Id));
+        }
+            
+        if (iteration != 2)
+            return;
+        
+        Musics2.Add(music);
+        Musics2.Sort((x, y) => (int)(x.Id - y.Id));
     }
 
 
